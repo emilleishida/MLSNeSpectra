3b60018a3e40ba359baafbf8befe6626945e327d
sasdelli <sasdelli@mpa-garching.mpg.de>
2015-09-26 14:45:23 +0200

    Name list.

--- python/create_data/select_data.py
+++ python/create_data/select_data.py
@@ -13,6 +13,7 @@ mean_error_treshold = 0.0011
 
 derivatives_orig = loadtxt(in_data_dir+'derivatives_orig.dat')
 errors_orig = loadtxt(in_data_dir+'errors_orig.dat')
+SNe_orig = loadtxt(in_data_dir+'SNe_orig.txt',dtype=str)
 
 derivatives = derivatives_orig.T[wavelength_range_sel].T
 errors = errors_orig.T[wavelength_range_sel].T
@@ -25,6 +26,14 @@ savefig(out_dir+'errors.png')
 sel_sne = [i for i in range(shape(errors)[0]) if mean(errors[i]) < mean_error_treshold]
 derivatives = derivatives[sel_sne]
 errors = errors[sel_sne]
+SNe = SNe_orig[sel_sne]
 
 savetxt(out_data_dir+'derivatives.dat', derivatives, header='Derivatives of the Log10 of the fluxes. Rows: SNe, Columns: Wavelengths')
 savetxt(out_data_dir+'errors.dat', errors, header='Errors on the derivatives. Rows: SNe, Columns: Wavelengths')
+
+f = open(out_data_dir+'SNe.txt', 'w')
+f.write('# Supernova names of derivative.dat\n')
+for item in SNe:
+  f.write("%s\n" % item)
+f.close()
+

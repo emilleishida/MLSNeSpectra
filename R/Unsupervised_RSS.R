8a7b8f641c6fa161582099f8d6b558637d1f29f1
Rafael <rafael.2706@gmail.com>
2015-09-26 12:13:52 +0200

    projection

--- R/Unsupervised_RSS.R
+++ R/Unsupervised_RSS.R
@@ -1,3 +1,13 @@
 # First try Unsupervised 
-
+require(RDRToolbox)
+require(mclust)
 spect<-read.table("..//data/derivatives.dat",header=F)
+
+s<-as.matrix(spect)
+proj = LLE(data=s, dim=3, k=10)
+
+mod = Mclust(as.data.frame(proj))
+summary(mod, parameters = TRUE)
+
+labels=mod$classification
+plotDR(data=proj,labels=labels)

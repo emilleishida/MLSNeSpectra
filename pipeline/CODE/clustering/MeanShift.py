4e57041143efc6a00a7666ebe1a54fe08366f35e
Hugo Camacho <hocamachoc@gmail.com>
2015-10-21 08:54:55 -0200

    redoing MeanShift

--- pipeline/CODE/clustering/MeanShift.py
+++ pipeline/CODE/clustering/MeanShift.py
@@ -1,14 +1,18 @@
-from __future__ import print_function
-
-from sklearn.cluster import KMeans
-from sklearn.cluster import MeanShift,estimate_bandwidth
+from sklearn.cluster import MeanShift, estimate_bandwidth
 
 
-def func(data,pars):
-	print ()
-	if len(pars)==0: clusters=MeanShift()
-	else:
-		quantile=pars[0]
-		clusters = MeanShift(bandwidth=estimate_bandwidth(data,quantile=quantile))
-	clusters.fit(data)
-	return clusters.cluster_centers_.T , clusters.labels_
+def clustering(data, params):
+
+    # parse arguments
+
+    if len(params) == 0:
+        quantile = 0
+    else:
+        quantile = params[0]
+
+    # apply Mean Shift to reduced data
+
+    clusters = MeanShift(bandwidth=estimate_bandwidth(data), quantile=quantile)
+    clusters.fit(data)
+
+    return [clusters.cluster_centers_.T, clusters.labels_]

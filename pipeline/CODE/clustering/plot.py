2155bacf172452efa30ea6018a592ba6c27fe7ca
Naminoshi <michel.aguena@gmail.com>
2015-10-21 17:05:42 -0200

    add label in plot

--- pipeline/CODE/clustering/plot.py
+++ pipeline/CODE/clustering/plot.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import numpy as np
 import matplotlib.pyplot as plt
+from config import REDUCTION_METHOD, CLUSTERING_METHOD 
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -40,7 +41,7 @@ def plot_clustering(cl_func,data,CLUSTERING_OUT_NAME='clustering.png'):
 				dat	= np.array([data[j],data[ind(i)]])
 				if fit_all	: colors = clust_all[1].astype(np.float)
 				elif fit_ind	: colors = clust_ind[ind(i)][j][1].astype(np.float)
-				PLT.scatter(dat[0],dat[1],c=colors,marker='o',label='data',lw=0,s=5)
+				PLT.scatter(dat[0],dat[1],c=colors,marker='o',label='data',lw=0,s=8)
 				if i!=j or (use_diag==False):
 					if fit_all: PLT.scatter(clust_all[0][j],clust_all[0][ind(i)],c='m',label='cluster centers',marker='x')
 					if fit_ind:
@@ -48,5 +49,6 @@ def plot_clustering(cl_func,data,CLUSTERING_OUT_NAME='clustering.png'):
 						PLT.scatter(centers[0],centers[1],color='c',label='cluster centers',marker='x',linewidths=1)
 	for j in range(Nplt):	plts[Nplt-1][j].set_xlabel('$PC_'+str(j+1)+'$')
 	for i in range(Nplt):	plts[i][0].set_ylabel('$PC_'+str(ind(i+1))+'$')
+	plts[0][Nplt-1].text(0,0,'REDUCTION_METHOD:\n  '+REDUCTION_METHOD+'\nCLUSTERING_METHOD:\n  '+CLUSTERING_METHOD,bbox={'facecolor':'1.', 'alpha':0.5, 'pad':20})
 	plt.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.15,hspace=0,wspace=0)
 	plt.savefig('plots/'+CLUSTERING_OUT_NAME)

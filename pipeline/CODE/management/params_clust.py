0f5a6b99b442f2160d787aef037e385dcd5ad879
vbusti <viniciusbusti@gmail.com>
2015-10-21 12:03:10 -0200

    keys to the clustering methods added

--- pipeline/CODE/management/params_clust.py
+++ pipeline/CODE/management/params_clust.py
@@ -1,17 +1,34 @@
 from __future__ import print_function
 MeanShift_dict=dict([
 	[ 'quantile'	, .25	],
-	[ 'quantile1'	, .251	],
-	[ 'quantile2'	, .225	],
-	[ 'quantile3'	, .235	],
+	[ 'cluster_all'	, True	],
 ])
 
 KMeans_dict=dict([
-	[ 'n_clusters'	, 4	],
+	[ 'n_clusters'	, 4	      ],
+        [ 'tol'         , 1e-4        ],
+        [ 'init'        , "'kmeans++'"],
+        [ 'n_jobs'      , 1           ],
 ])
 
 AgglomerativeClustering_dict=dict([
 	[ 'n_clusters'	, 6		],
 	[ 'affinity'	, '"euclidean"'	],
 	[ 'linkage'	, '"ward"'	],
+])
+
+AffinityPropagation_dict=dict([
+        [ 'preference'      , None         ],
+        [ 'convergence_iter', 15           ],
+        [ 'max_iter'        , 200          ],
+        [ 'damping'         , 0.5          ],
+        [ 'affinity'        , "'euclidean'"],
+])
+
+DBSCAN_dict=dict([
+        [ 'eps'          ,   0.5           ],
+        [ 'min_samples'  ,   5             ],
+        [ 'metric'       ,   "'euclidean'" ],
+        [ 'algorithm'    ,   "'auto'"      ],
+        [ 'leaf_size'    ,   30            ],
 ])

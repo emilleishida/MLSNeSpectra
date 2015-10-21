670b03b1f08618c4a99d4248e303931a382cdc75
drArli <arlindo.trindade@gmail.com>
2015-10-21 20:52:37 +0100

    New code!

--- python/empca/run_empca.py
+++ python/empca/run_empca.py
@@ -1,51 +1,29 @@
 import os
-<<<<<<< HEAD
 import tarfile
-=======
->>>>>>> Trindade_dev
 from numpy import loadtxt, mean,ones
 from pylab import figure
 from matplotlib.pyplot import plot, savefig, xlabel, ylabel, scatter, axis, xlim
-from empca import empca
 import sys
 
 data_dir='../../data_all_types'
 out_dir='./plots/'
 
-<<<<<<< HEAD
-url_tar = "https://www.dropbox.com/s/q22paf70tyulvka/derivatives.dat.tgz?dl=0"
-url_empca = "https://raw.githubusercontent.com/sbailey/empca/master/empca.py"
-
-
-# load the data and run PCA
-
-if "derivatives.dat" not in os.listdir("./"):
-	
-	os.system( "wget %s"%url_tar )
-	
-	os.system( "tar -xzf derivatives.dat.tgz*")
-	os.system( "rm derivatives.dat.tgz?dl=0" )
-=======
 
 url_empca = "https://raw.githubusercontent.com/sbailey/empca/master/empca.py"
 
 
 # Download empca.py file
->>>>>>> Trindade_dev
+
 
 if "empca.py" not in os.listdir("./"):
 	
 	os.system(  "wget %s"%url_empca )
+
+from empca import empca	
 	
-	
 
+derivatives = loadtxt(os.path.join(data_dir,'derivatives.dat') )
 
-
-<<<<<<< HEAD
-derivatives = loadtxt('derivatives.dat')
-=======
-derivatives = loadtxt(os.path.join(data_dir,'oderivatives.dat')
->>>>>>> Trindade_dev
 
 
 errors = ones(derivatives.shape)

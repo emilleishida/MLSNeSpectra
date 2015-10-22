677973de9e008159cb48062db808f2a7a24c3255
Hugo Camacho <hocamachoc@gmail.com>
2015-10-22 09:51:11 -0200

    updating ability to pass string-type arguments

--- pipeline/CODE/management/params_clust.py
+++ pipeline/CODE/management/params_clust.py
@@ -7,14 +7,14 @@ MeanShift_dict=dict([
 KMeans_dict=dict([
 	[ 'n_clusters'	, 4	       ],
         [ 'tol'         , 1e-4         ],
-        [ 'init'        , "'k-means++'"],
+        [ 'init'        , 'k-means++'],
         [ 'n_jobs'      , 1            ],
 ])
 
 AgglomerativeClustering_dict=dict([
 	[ 'n_clusters'	, 6		],
-	[ 'affinity'	, '"euclidean"'	],
-	[ 'linkage'	, '"ward"'	],
+	[ 'affinity'	, 'euclidean'	],
+	[ 'linkage'	, 'ward'	],
 ])
 
 AffinityPropagation_dict=dict([
@@ -22,13 +22,13 @@ AffinityPropagation_dict=dict([
         [ 'convergence_iter', 15           ],
         [ 'max_iter'        , 200          ],
         [ 'damping'         , 0.5          ],
-        [ 'affinity'        , "'euclidean'"],
+        [ 'affinity'        , 'euclidean'],
 ])
 
 DBSCAN_dict=dict([
         [ 'eps'          ,   0.5           ],
         [ 'min_samples'  ,   5             ],
-        [ 'metric'       ,   "'euclidean'" ],
-        [ 'algorithm'    ,   "'auto'"      ],
+        [ 'metric'       ,   'euclidean' ],
+        [ 'algorithm'    ,   'auto'      ],
         [ 'leaf_size'    ,   30            ],
 ])

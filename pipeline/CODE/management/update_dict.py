05c5103d0c97221a1517a44550c631bce406d585
Naminoshi <michel.aguena@gmail.com>
2015-10-22 22:30:52 -0200

    fixed old prints

--- pipeline/CODE/management/update_dict.py
+++ pipeline/CODE/management/update_dict.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from config import *
 
 def update_dict(DICT,prefix):
+	print('** CHECKING FOR',prefix,'UPDATES **')
+	updates=False
 	for item0 in DICT:
 		item=prefix+item0
 		try:
@@ -9,7 +11,8 @@ def update_dict(DICT,prefix):
 		except NameError:
 			pass
 		else:
-  			print (item0,"\t was updated from",DICT[item0],"to",eval(item))
+  			print ('\t-',item0,"\t was updated from",DICT[item0],"to",eval(item))
 			DICT[item0]=eval(item)
-			
-
+			updates=True
+	if updates==False: print('\t<no updates>')
+	print('')

385237200e15e56c407a06500158a58504287283
Naminoshi <michel.aguena@gmail.com>
2015-10-30 10:54:47 -0200

    fix bug in cl output names

--- pipeline/CODE/management/name_files.py
+++ pipeline/CODE/management/name_files.py
@@ -1,5 +1,5 @@
 def create_name(Method,params,prefix,end='.dat'):
-    fname = 'red_data/reduced_data_' +Method+ end
+    fname = prefix +Method+ end
     return fname
 def plot_name(Method_red,Method_cl,params_red,params_cl,ext='.png'):
     fname = 'plots/plot_' + Method_red + '_' + Method_cl +ext

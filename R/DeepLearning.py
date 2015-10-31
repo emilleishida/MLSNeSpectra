fbd856a07f9b9f7891b72224037ee2c281e04a80
sasdelli <sasdelli@mpa-garching.mpg.de>
2015-10-31 15:25:26 +0000

    Deep Learning with seed

--- R/DeepLearning.py
+++ R/DeepLearning.py
@@ -5,10 +5,18 @@ import numpy as np
 in_file = "../data_all_types/derivatives.dat"
 
 for input_string in '120,100,90,50,30,20,2,20,30,50,90,100,120', '120,100,90,50,30,20,3,20,30,50,90,100,120', '120,100,90,50,30,20,4,20,30,50,90,100,120', '120,100,90,50,30,20,5,20,30,50,90,100,120', '120,100,90,50,30,20,6,20,30,50,90,100,120', '120,100,90,50,30,20,7,20,30,50,90,100,120', '120,100,90,50,30,20,8,20,30,50,90,100,120', '120,100,90,50,30,20,9,20,30,50,90,100,120':
+    #for input_string in ['120,100,90,50,30,20,2,20,30,50,90,100,120']:
+    #for seed in [1,2,3,4,5,6]:
+    seed = 1
+    #input_string = '120,100,90,50,30,20,2,20,30,50,90,100,120'
+
+    #for input_string in ['100,50,2,50,100']:
+    #for input_string in ['120,100,90,50,30,20,5,2,5,20,30,50,90,100,120']:
     
-    in_param = 'training_frame = train.hex, activation = "Tanh", autoencoder=T, hidden = c(%s), epochs=100, ignore_const_cols = F'  % (input_string)
+    in_param = 'training_frame = train2.hex, activation = "Tanh", autoencoder=T, hidden = c(%s), epochs=100, ignore_const_cols = F, seed = %d, reproducible = TRUE '  % (input_string,seed)
     #in_param = 'training_frame = train.hex, activation = "Tanh", autoencoder=T, hidden = c(120,100,90,50,30,20,10,20,30,50,90,100,120), epochs=100, ignore_const_cols = F'  
     n_layers = 7
+    #n_layers = 3
     #in_param = 'training_frame = train.hex, activation = "Tanh", autoencoder=T, hidden = c(180,120,100,90,70,50,40,30,20,10,20,30,40,50,70,90,100,120,180), epochs=300, ignore_const_cols = F'  
     #n_layers = 10
     
@@ -28,18 +36,18 @@ for input_string in '120,100,90,50,30,20,2,20,30,50,90,100,120', '120,100,90,50,
     
     r('TRAIN = "%s" '% (in_file_cvs))
     
-    r('train.hex <- h2o.importFile(h2oServer, path = TRAIN, header = F, sep = ",", destination_frame="train.hex")')
+    r('train2.hex <- h2o.importFile(h2oServer, path = TRAIN, header = F, sep = ",", destination_frame="train2.hex")')
     
     ## Construct deep learning model
     r('dlmodel <- h2o.deeplearning( %s ) ' % (in_param))
     
     ## Generate new features
-    r('features_dl <- h2o.deepfeatures(dlmodel, train.hex, layer=%d)' % (n_layers))
+    r('features_dl <- h2o.deepfeatures(dlmodel, train2.hex, layer=%d)' % (n_layers))
     r('head(features_dl)')
     
     ## Store new features in file
     X = r('as.matrix(features_dl)')
-    np.savetxt('./out_DeepLearning/out_%s_dl.dat' % (input_string)  ,X)
+    np.savetxt('./out_DeepLearning/out_%s_seed%d_dl.dat' % (input_string,seed)  ,X)
     
     
     
@@ -75,6 +83,6 @@ for input_string in '120,100,90,50,30,20,2,20,30,50,90,100,120', '120,100,90,50,
     
     ## Store new predictions in file
     pred = r('as.matrix(predictions_dl)')
-    np.savetxt('./out_DeepLearning/predictions_%s_dl.dat' % (input_string) ,pred)
+    np.savetxt('./out_DeepLearning/predictions_%s_seed%d_dl.dat' % (input_string,seed) ,pred)
 
   

7a3656f3dd4fdb871ad7d5fe7a99249e7059307f
sasdelli <sasdelli@mpa-garching.mpg.de>
2015-11-18 22:37:18 +0000

    plot on transfer learning

--- python/pca/run_pca.py
+++ python/pca/run_pca.py
@@ -1,9 +1,10 @@
 from sklearn.decomposition.pca import PCA
-from numpy import loadtxt
+from numpy import loadtxt, savetxt
 from pylab import figure
 from matplotlib.pyplot import plot, savefig, xlabel, ylabel, scatter, axis, xlim
 
-data_dir='../../data/'
+#data_dir='../../data/'
+data_dir='../../data_all_types/'
 out_dir='./plots/'
 
 # load the data and run PCA
@@ -20,12 +21,13 @@ xlabel('PC_n')
 ylabel('Variance (%)')
 savefig(out_dir+'variance_plot.png')
 
-for indexes in [[0,1],[0,2],[0,3],[0,4]]:
-    figure()
-    scatter(X[:, indexes[0]], X[:, indexes[1]])
-    xlabel('PC %d' % (indexes[0]+1))
-    ylabel('PC %d' % (indexes[1]+1))
-    axis('equal')
-    savefig(out_dir+'scatter_plot_%d_%d.png' % (indexes[0]+1,indexes[1]+1))
+#for indexes in [[0,1],[0,2],[0,3],[0,4]]:
+#    figure()
+#    scatter(X[:, indexes[0]], X[:, indexes[1]])
+#    xlabel('PC %d' % (indexes[0]+1))
+#    ylabel('PC %d' % (indexes[1]+1))
+#    axis('equal')
+#    savefig(out_dir+'scatter_plot_%d_%d.png' % (indexes[0]+1,indexes[1]+1))
 
+savetxt('output_coeff.dat', X)
 
---
+++ python/transfer_learning_test/plot_variances.py
@@ -0,0 +1,28 @@
+
+from numpy import loadtxt, genfromtxt, savetxt, var, random
+from pylab import figure
+from matplotlib.pyplot import plot, savefig, xlabel, ylabel, scatter, axis, xlim, ylim, legend
+
+data_to_plot = loadtxt('./residuals.dat').T
+figure()
+plot(data_to_plot[0], data_to_plot[1],'xr', label='PCA at max')
+plot(data_to_plot[0], data_to_plot[2],'Dr', label='PCA with TL')
+plot(data_to_plot[0], data_to_plot[3],'xk', label='DL at max')
+plot(data_to_plot[0], data_to_plot[4],'Dk', label='DL with TL')
+ylabel('fractional variance')
+xlabel('features')
+ylim(0,1)
+legend()
+
+savefig('./plots/transfer_learning.png')
+
+data_to_plot = loadtxt('./residuals_all_matrix.dat').T
+figure()
+plot(data_to_plot[0], data_to_plot[1],'or', label='PCA with all epochs')
+plot(data_to_plot[0], data_to_plot[2],'ok', label='DL with all epochs')
+ylabel('fractional variance')
+xlabel('features')
+ylim(0,1)
+
+savefig('./plots/PCA_vs_DL.png')
+
